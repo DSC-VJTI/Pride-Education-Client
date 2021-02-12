30a42bcb070e2191423d8bcfbcba9827437a0ef7
Shaikh Mohammed Ammar <shaikhmohammedammar62@gmail.com>
2021-02-12 15:05:39 +0530

    Some changes to be made in market places made a skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Products from "./components/Products";
+import NavbarPage from "./components/NavbarTop";
+import Footer from "./components/FooterBottom"
+import Pane from "./components/ClassesPane"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarPage></NavbarPage>
+      <Products></Products>
+      <Pane></Pane>
+      <Pane></Pane>
+      <Pane></Pane>
+      <Pane></Pane>
+      <Pane></Pane>
+      <Footer></Footer>
     </div>
   );
 }
