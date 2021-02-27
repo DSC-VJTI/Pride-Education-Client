ca31d03bd28e04ae9570b27c25c45735bb79d8aa
Pranav Pandey <52607038+pranav750@users.noreply.github.com>
2021-02-27 10:27:42 +0530

    Schemas added (#7)
    * Schemas added
    
    * Product Schema fixed

---
+++ server/src/models/Cart/ICart.ts
@@ -0,0 +1,8 @@
+import { Document } from "mongoose";
+import IProduct from "../Product/IProduct";
+import IUser from "../User/IUser";
+
+export default interface ICart extends Document {
+  user: IUser["_id"];
+  products: Array<IProduct["_id"]>;
+}
