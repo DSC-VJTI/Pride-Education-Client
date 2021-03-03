2d3ca9fd99fa4852eb33b3fdedba758ccd75e64a
BhavyaSheth22 <brsheth_b18@it.vjti.ac.in>
2021-03-03 19:08:15 +0530

    added dev setup - morgan and cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 import dotenv from "dotenv";
 import router from "./routes";
 import dbConnect from "./config/dbconnect";
@@ -8,10 +10,19 @@ const app = express();
 
 dbConnect();
 
+app.use(express.json({ limit: "10mb" }));
+app.use(cors());
+
+if (process.env.NODE_ENV !== "test") {
+  app.use(morgan("combined"));
+}
+
 app.use("/api", router);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
 
 export default app;
