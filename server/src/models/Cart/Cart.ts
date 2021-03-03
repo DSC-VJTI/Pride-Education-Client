b6722feb842c3dfc4b1e8da45d7f9a8085281409
BhavyaSheth22 <brsheth_b18@it.vjti.ac.in>
2021-03-03 09:25:38 +0530

    fix lint errors in pretest

--- server/src/models/Cart/Cart.ts
+++ server/src/models/Cart/Cart.ts
@@ -3,7 +3,7 @@ import ICart from "./ICart";
 
 const CartSchema: Schema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User" },
-  products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+  products: [{ type: Schema.Types.ObjectId, ref: "Product" }]
 });
 
 export default model<ICart>("Cart", CartSchema);
