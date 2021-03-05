ea94b472d289dc9088c4d8f09485db2c147775e6
Pranav <pranavpandey197@gmail.com>
2021-03-05 12:29:04 +0530

    Product controller added

---
+++ server/src/controllers/product.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import Product from "../models/Product/Product";
+
+export const getProducts = async (_: any, res: Response) => {
+  try {
+    const data = await Product.find({});
+
+    return res
+      .status(200)
+      .json({ data, message: "Successfully found products" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const data = await Product.findById(id);
+
+    if (data) {
+      return res.status(200).json({ data });
+    } else {
+      return res.status(404).json({ message: "Product doesn't exists" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
