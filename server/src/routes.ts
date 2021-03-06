37e67c1e52143ef429d4c312cabffdb3873e5b07
Pranav <pranavpandey197@gmail.com>
2021-03-06 15:22:09 +0530

    Product routes added

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import AuthController from "./controllers/auth.controller";
+import { getProducts, getProductById } from "./controllers/product.controller";
 import auth from "./middleware/auth.middleware";
 
 const router = express.Router();
@@ -18,5 +19,10 @@ router.post(
 router.post("/register", auth.verifyOTP, AuthController.register);
 router.post("/sendOtpLogin", auth.userExists("login"), AuthController.sendOTP);
 router.post("/login", auth.verifyOTP, AuthController.login);
+
+// Product routes
+router.get("/products", getProducts);
+router.get("/products/:id", getProductById);
+router.get("/products/filter/:id", getProductById);
 
 export default router;
