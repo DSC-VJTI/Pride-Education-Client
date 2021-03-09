26865776dbadba6b1b2bcfe5c1752d2d3ffee636
pal-16 <mantrypalak@gmail.com>
2021-03-09 15:13:23 +0530

    changed functions structure and added types to controllers

--- server/src/controllers/admin.controller.ts
+++ server/src/controllers/admin.controller.ts
@@ -1,65 +1,85 @@
 import User from "../models/User/User";
 import Product from "../models/Product/Product";
 
-export const newProduct = async (req, res, next) => {
-  try {
-    const product = await Product.create({ ...req.body });
-    product.save().catch((err) => console.log("Error saving product ", err));
-    return res.status(200).json({
+import express from "express";
+
+const AdminController = {
+  async newProduct(
+    req: express.Request,
+    res: express.Response,
+    next
+  ): Promise<express.Response> {
+    try {
+      const product = await Product.create({ ...req.body });
+      product.save().catch((err) => console.log("Error saving product ", err));
+      return res.status(200).json({
+        product
+      });
+    } catch (err) {
+      return next({
+        status: 400,
+        message: err.message
+      });
+    }
+  },
+
+  async editProduct(
+    req: express.Request,
+    res: express.Response,
+    next
+  ): Promise<express.Response> {
+    try {
+      const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        upsert: true,
+        new: true
+      });
       product
-    });
-  } catch (err) {
-    return next({
-      status: 400,
-      message: err.message
-    });
+        .save()
+        .catch((err) => console.log("Error updating product ", err));
+      return res.status(200).json({
+        message: "Product Updated Successfully"
+      });
+    } catch (err) {
+      return next({
+        status: 400,
+        message: err.message
+      });
+    }
+  },
+
+  async deleteProduct(
+    req: express.Request,
+    res: express.Response,
+    next
+  ): Promise<express.Response> {
+    try {
+      await Product.findByIdAndDelete(req.params.id);
+      return res.status(200).json({
+        message: "Successfully Deleted"
+      });
+    } catch (err) {
+      return next({
+        status: 400,
+        message: err.message
+      });
+    }
+  },
+
+  async allUsers(res: express.Response, next): Promise<express.Response> {
+    try {
+      const allUsers = await User.find({});
+      return res.status(200).json({
+        allUsers: allUsers.map((user) => {
+          return user.toObject();
+        })
+      });
+    } catch (err) {
+      return next({
+        status: 400,
+        message: err.message
+      });
+    }
   }
 };
 
-export const editProduct = async (req, res, next) => {
-  try {
-    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-      upsert: true,
-      new: true
-    });
-    product.save().catch((err) => console.log("Error updating product ", err));
-    return res.status(200).json({
-      message: "Product Updated Successfully"
-    });
-  } catch (err) {
-    return next({
-      status: 400,
-      message: err.message
-    });
-  }
-};
-
-export const deleteProduct = async (req, res, next) => {
-  try {
-    await Product.findByIdAndDelete(req.params.id);
-    return res.status(200).json({
-      message: "Successfully Deleted"
-    });
-  } catch (err) {
-    return next({
-      status: 400,
-      message: err.message
-    });
-  }
-};
-
-export const allUsers = async (_req, res, next) => {
-  try {
-    const allUsers = await User.find({});
-    return res.status(200).json({
-      allUsers: allUsers.map((user) => {
-        return user.toObject();
-      })
-    });
-  } catch (err) {
-    return next({
-      status: 400,
-      message: err.message
-    });
-  }
-};
+export = AdminController;
