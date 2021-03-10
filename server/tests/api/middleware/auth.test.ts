aa47e8fd7f200115fcba4be5950601c143a641b3
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-03-10 18:29:20 +0530

    finish auth tests

--- server/tests/api/middleware/auth.test.ts
+++ server/tests/api/middleware/auth.test.ts
@@ -6,10 +6,16 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 import app from "../../../src/app";
+import IUser from "../../../src/models/User/IUser";
+import User from "../../../src/models/User/User";
 import { generateOtpHash } from "../../../src/utility/otp";
 
 
 describe("middleware/auth tests", () => {
+
+    afterEach((done) => {
+        User.deleteMany().then(() => done())
+    })
 
     describe('verify OTP', () => {
         let otp: number;
@@ -30,8 +36,34 @@ describe("middleware/auth tests", () => {
                     expect(err).to.be.null;
                     expect(res.status).to.be.equal(401);
                     expect(res.body).to.be.an("object");
+                    expect(res.body).to.have.property("error").equal("Incorrect OTP");
                     done();
                 });
         });
+    });
+
+    describe('userExists', () => {
+
+        it("returns 422 if user exists and wants to register again", (done) => {
+            const user = new User({
+                name: 'test user',
+                email: 'test@dsc.in',
+                mobileNumber: 9874563210,
+                address: 'Chandni Chowk'
+            });
+            user.save().then((savedUser: IUser) => {
+                chai.request(app)
+                    .post("/api/sendOtpRegister")
+                    .send({ email: savedUser.email })
+                    .end((err, res) => {
+                        expect(err).to.be.null;
+                        expect(res.status).to.be.equal(422);
+                        expect(res.body).to.be.an("object");
+                        expect(res.body).to.have.property("error").equal("Email already exists");
+                        done();
+                    })
+            })
+        })
     })
+
 });
