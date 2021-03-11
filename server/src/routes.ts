0a5bc103641ac7a9ae6d2d4eb4df560144ab454c
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-03-11 12:06:56 +0530

    remove code duplicacy but keep code

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import AuthController from "./controllers/auth.controller";
 import auth from "./middleware/auth.middleware";
 import admin from "./controllers/admin.controller";
+import adminMinddleware from "./middleware/admin.middleware";
 const router = express.Router();
 
 // Basic route
@@ -19,9 +20,29 @@ router.post("/register", auth.verifyOTP, AuthController.register);
 router.post("/sendOtpLogin", auth.userExists("login"), AuthController.sendOTP);
 router.post("/login", auth.verifyOTP, AuthController.login);
 
-router.post("/admin/product/:userId", auth.isAdmin, admin.newProduct);
-router.put("/admin/product/:userId/:id", auth.isAdmin, admin.editProduct);
-router.delete("/admin/product/:userId/:id", auth.isAdmin, admin.deleteProduct);
-router.get("/admin/users/:userId", auth.isAdmin, admin.getUsers);
+router.post(
+  "/admin/product/:userId",
+  auth.isAuthenticated,
+  adminMinddleware.isAdmin,
+  admin.newProduct
+);
+router.put(
+  "/admin/product/:userId/:id",
+  auth.isAuthenticated,
+  adminMinddleware.isAdmin,
+  admin.editProduct
+);
+router.delete(
+  "/admin/product/:userId/:id",
+  auth.isAuthenticated,
+  adminMinddleware.isAdmin,
+  admin.deleteProduct
+);
+router.get(
+  "/admin/users/:userId",
+  auth.isAuthenticated,
+  adminMinddleware.isAdmin,
+  admin.getUsers
+);
 
 export default router;
