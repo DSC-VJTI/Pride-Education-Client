55c9dff6a8ebfe85d814c810d3a4cafa4c0670d2
Shaikh Mohammed Ammar <shaikhmohammedammar62@gmail.com>
2021-03-11 20:35:05 +0530

    Resourse and Course Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import LandingPage from "./components/LandingPage/LandingPage";
-
+import ClassesPane from "./components/Resource Page/ClassesPane";
+import CoursePage from "./components/Product Page/CoursePage";
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
+        <Route path="/classes" component={ClassesPane} />
+        <Route path="/product" component={CoursePage} />
         <Route path="/" component={LandingPage} />
       </Switch>
     </Router>
