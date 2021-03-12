5c1b381cf7d3926602441325cd9721b967af3efd
MahekNakhua <maheknakhua11@gmail.com>
2021-03-12 14:44:36 +0530

    get changed to post route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -21,7 +21,7 @@ router.post("/sendOtpLogin", auth.userExists("login"), AuthController.sendOTP);
 router.post("/login", auth.verifyOTP, AuthController.login);
 
 //cart routes
-router.get("/cart", CartController.showCart);
+router.post("/cart", CartController.showCart);
 router.post("/cart/:productId", CartController.addToCart);
 router.delete("/cart/:productId", CartController.removeFromCart);
 
