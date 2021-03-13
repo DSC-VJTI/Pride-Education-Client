79945325fbeaffbe3b448b9a6b14c1d81efb8cd2
Shaikh Mohammed Ammar <shaikhmohammedammar62@gmail.com>
2021-03-13 18:02:52 +0530

    Coloring resource and course page

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,14 @@ import Navbar from "./components/Navbar/Navbar";
 import LandingPage from "./components/LandingPage/LandingPage";
 import ClassesPane from "./components/Resource Page/ClassesPane";
 import CoursePage from "./components/Product Page/CoursePage";
+
+
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
+
         <Route path="/classes" component={ClassesPane} />
         <Route path="/product" component={CoursePage} />
         <Route path="/" component={LandingPage} />
