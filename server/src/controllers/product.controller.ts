c09ba0b6552c78d146a9dbe41752bdc6eaee6f66
Pranav Pandey <52607038+pranav750@users.noreply.github.com>
2021-03-13 18:12:40 +0530

    Product controller and routes added (#18)
    * Product controller added
    
    * Product controller status codes fixed
    
    * Product routes added
    
    * Product controllers errors fixed
    
    * Status codes updated

---
+++ server/src/controllers/product.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import Product from "../models/Product/Product";
+
+const ProductController = {
+  async getProducts(_: Request, res: Response): Promise<Response> {
+    try {
+      const data = await Product.find({});
+
+      return res.status(200).json({ data });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  },
+
+  async getProductById(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params;
+      const data = await Product.findById(id);
+
+      if (data) {
+        return res.status(200).json({ data });
+      } else {
+        return res.status(404).json({ message: "Product doesn't exists" });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  },
+
+  async getProductsByFilter(req: Request, res: Response): Promise<Response> {
+    try {
+      const data = await Product.find(req.body);
+
+      return res.status(200).json({ data });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+};
+
+export = ProductController;
