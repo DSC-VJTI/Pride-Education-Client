06dac1287174ed1750998dfe9fd9e9fffd0b2153
Shubhankar Kanchan Gupta <shubhankar.gupto.11@gmail.com>
2021-03-19 17:28:56 +0530

    Model/product (#36)
    * update product schema
    
    * fux typo
    
    * fix model structure
    
    * fix controllers

--- server/src/controllers/admin.controller.ts
+++ server/src/controllers/admin.controller.ts
@@ -1,7 +1,42 @@
-import User from "../models/User/User";
-import Product from "../models/Product/Product";
-
 import express from "express";
+import Book from "../models/Product/Book";
+import Course from "../models/Product/Course";
+import Test from "../models/Product/Test";
+import Product from "../models/Product/Product";
+import User from "../models/User/User";
+
+interface ICourseBody {
+  level: string;
+  subject: string;
+  faculty: string;
+  type: string;
+  applicableExamDate: Date;
+  language: string;
+  duration: number;
+  sysReq: string;
+  views: number;
+  validity: number;
+  mode: string;
+}
+
+interface ITestBody {
+  subject: string;
+  contents: string;
+}
+
+interface IBookBody {
+  url: string;
+}
+
+interface IProductBody {
+  name: string;
+  price: number;
+  discount: number;
+  type: "course" | "test" | "book";
+  course?: ICourseBody;
+  test?: ITestBody;
+  book?: IBookBody;
+}
 
 const AdminController = {
   async newProduct(
@@ -9,7 +44,25 @@ const AdminController = {
     res: express.Response
   ): Promise<express.Response> {
     try {
-      const product = await Product.create({ ...req.body });
+      const { name, discount, price, type } = req.body as IProductBody;
+      let product = new Product({ name, discount, type, price });
+      switch (type) {
+        case "course":
+          product.course = await Course.create(req.body.course);
+          break;
+        case "book":
+          product.book = await Book.create(req.body.book);
+          break;
+        case "test":
+          product.test = await Test.create(req.body.test);
+          break;
+        default:
+          return res.status(400).json({
+            error:
+              "Any one out of course, test or book expected in res.body.type"
+          });
+      }
+      product = await product.save();
       return res.status(201).json({
         product
       });
@@ -25,18 +78,37 @@ const AdminController = {
     res: express.Response
   ): Promise<express.Response> {
     try {
-      const doesProductExist = await Product.exists({
-        _id: req.params.productId
-      });
-      if (doesProductExist) {
-        const product = await Product.findByIdAndUpdate(
-          req.params.productId,
-          req.body,
-          {
-            upsert: true,
-            new: true
-          }
-        );
+      const product = await Product.findById(req.params.productId);
+      if (product) {
+        const { type } = req.body;
+        switch (type) {
+          case "course":
+            product.course = await Course.findByIdAndUpdate(
+              product.course._id,
+              req.body.course,
+              { upsert: true, new: true }
+            );
+            break;
+          case "book":
+            product.book = await Book.findByIdAndUpdate(
+              product.book._id,
+              req.body.book,
+              { upsert: true, new: true }
+            );
+            break;
+          case "test":
+            product.test = await Test.findByIdAndUpdate(
+              product.test._id,
+              req.body.test,
+              { upsert: true, new: true }
+            );
+            break;
+          default:
+            return res.status(400).json({
+              error:
+                "Any one out of course, test or book expected in res.body.type"
+            });
+        }
         return res.status(200).json({
           product,
           message: "Product Updated Successfully"
@@ -56,10 +128,15 @@ const AdminController = {
     res: express.Response
   ): Promise<express.Response> {
     try {
-      const doesProductExist = await Product.exists({
-        _id: req.params.productId
-      });
-      if (doesProductExist) {
+      const product = await Product.findById(req.params.productId);
+      if (product) {
+        if (product.course) {
+          await Course.findByIdAndDelete(product.course._id);
+        } else if (product.test) {
+          await Test.findByIdAndDelete(product.test._id);
+        } else {
+          await Book.findByIdAndDelete(product.book._id);
+        }
         await Product.findByIdAndDelete(req.params.productId);
         return res.status(200).json({
           message: "Successfully Deleted"
