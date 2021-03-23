00f8cdf8b06f57ee243258dc539f8faa848ee16d
Tejas Jhamnani <tejasj2909@gmail.com>
2021-03-23 21:14:55 +0530

    Added the Footer Section

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import ClassesPane from "./components/Resource Page/ClassesPane";
+import CoursePage from "./components/Product Page/CoursePage";
 import Navbar from "./components/Navbar";
 import Landing from "./components/LandingPage/Landing";
 import SupportPage from "./components/Support Page/SupportPage";
-import Login from "./components/LoginRegister/Login";
 import Registration from "./components/LoginRegister/Registration";
 import LoginRegister from "./components/LoginRegister/LoginRegister";
-import Footer from "./components/FooterSection/Footer";
+import Cart from "./components/Cart/Cart";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
@@ -16,17 +18,20 @@ function App() {
     Aos.init({ duration: 2000 });
   }, []);
   return (
-  <>
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Landing} />
-        <Route path="/support" component={SupportPage} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={LoginRegister} />
-      </Switch>
-    </Router>
-    <Footer />
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/classes" component={ClassesPane} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/product" component={CoursePage} />
+          <Route path="/support" component={SupportPage} />
+          <Route path="/register" component={LoginRegister} />
+          <Route path="/login" component={LoginRegister} />
+          <Route path="/" exact component={Landing} />
+        </Switch>
+      </Router>
+      <Footer />
     </>
   );
 }
