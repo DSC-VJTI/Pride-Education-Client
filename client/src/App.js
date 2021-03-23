6a198048d145ae021402dff9d19ba6ab05715234
Tejas Jhamnani <tejasj2909@gmail.com>
2021-03-23 18:38:00 +0530

    Added the Footer

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import SupportPage from "./components/Support Page/SupportPage";
 import Login from "./components/LoginRegister/Login";
 import Registration from "./components/LoginRegister/Registration";
 import LoginRegister from "./components/LoginRegister/LoginRegister";
+import Footer from "./components/FooterSection/Footer";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
@@ -15,6 +16,7 @@ function App() {
     Aos.init({ duration: 2000 });
   }, []);
   return (
+  <>
     <Router>
       <Navbar />
       <Switch>
@@ -24,6 +26,8 @@ function App() {
         <Route path="/register" component={LoginRegister} />
       </Switch>
     </Router>
+    <Footer />
+    </>
   );
 }
 
