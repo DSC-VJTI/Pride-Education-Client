7ddf822e1008fe737c360dbdc9ca439619530363
Tejas Jhamnani <tejasj2909@gmail.com>
2021-03-24 17:35:40 +0530

    Fixed the Alignment of Footer

--- client/src/components/Support Page/SupportPage.js
+++ client/src/components/Support Page/SupportPage.js
@@ -28,7 +28,7 @@ const SupportPage = () => {
 
   return (
     <Container maxWidth="sm">
-      <div className={classes.root}>
+      <div style={{ marginTop: "100px" }} className={classes.root}>
         <Tabs />
       </div>
     </Container>
