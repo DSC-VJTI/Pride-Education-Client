b92ef02b1f255f13a9cffca2a42dbfb4d874a559
Tejas Jhamnani <tejasj2909@gmail.com>
2021-04-02 12:57:21 +0530

    Added UI of Login Register And Footer

--- client/src/components/LoginRegister/Registration.jsx
+++ client/src/components/LoginRegister/Registration.jsx
@@ -1,13 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./css/FormStyle.css";
-import {
-  FormControl,
-  // Input,
-  InputLabel,
-  // Button,
-  Select,
-  MenuItem
-} from "@material-ui/core";
 import * as yup from "yup";
 import { Formik } from "formik";
 import OtpPage from "./inputOTP";
@@ -197,12 +189,6 @@ const Registration = () => {
                     </small>
                   ) : null}
                 </div>
-                {/* <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column"
-                }}
-              > */}
                 <div
                   className="inputField"
                   style={{ width: "92%", margin: "auto", position: "relative" }}
@@ -218,25 +204,6 @@ const Registration = () => {
                     menuItems={["CA", "CS", "B.COM"]}
                   />
                 </div>
-                {/* <FormControl
-                  className="inputFields"
-                  style={{ width: "40%", marginTop: "0.3rem" }}
-                >
-                  <InputLabel>Field</InputLabel>
-                  <Select
-                    name="field"
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.field}
-                  >
-                    <MenuItem value={"CA"}>CA</MenuItem>
-                    <MenuItem value={"CS"}>CS</MenuItem>
-                    <MenuItem value={"B.COM"}>B.COM</MenuItem>
-                  </Select>
-                  {props.touched.field ? (
-                    <small style={{ color: "red" }}>{props.errors.field}</small>
-                  ) : null}
-                </FormControl> */}
                 <div
                   className="inputField"
                   style={{ width: "92%", margin: "auto", position: "relative" }}
@@ -252,26 +219,6 @@ const Registration = () => {
                     menuItems={["Foundation", "ICPC", "Final"]}
                   />
                 </div>
-                {/* <FormControl
-                  className="inputFields"
-                  style={{ width: "40%", marginTop: "0.3rem" }}
-                >
-                  <InputLabel>Level</InputLabel>
-                  <Select
-                    name="level"
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.level}
-                  >
-                    <MenuItem value={"Foundation"}>Foundation</MenuItem>
-                    <MenuItem value={"ICPC"}>ICPC</MenuItem>
-                    <MenuItem value={"Final"}>Final</MenuItem>
-                  </Select>
-                  {props.touched.level ? (
-                    <small style={{ color: "red" }}>{props.errors.level}</small>
-                  ) : null}
-                </FormControl> */}
-                {/* </div> */}
                 <div
                   className="inputField"
                   style={{ width: "92%", margin: "auto", position: "relative" }}
@@ -287,97 +234,17 @@ const Registration = () => {
                     menuItems={["Social Media", "Word of mouth", "Other"]}
                   />
                 </div>
-                {/* <FormControl
-                  className="inputFields"
-                  style={{ width: "90%", marginTop: "0.3rem" }}
-                >
-                  <InputLabel>Reference</InputLabel>
-                  <Select
-                    name="reference"
-                    onChange={props.handleChange}
-                    onBlur={props.handleBlur}
-                    value={props.values.reference}
-                  >
-                    <MenuItem value={"Social Media"}>
-                      Through social media
-                    </MenuItem>
-                    <MenuItem value={"Word of mouth"}>
-                      Through recommendation by friends/family
-                    </MenuItem>
-                    <MenuItem value={"Other"}>Other</MenuItem>
-                  </Select>
-                  {props.touched.reference ? (
-                    <small style={{ color: "red" }}>
-                      {props.errors.reference}
-                    </small>
-                  ) : null}
-                </FormControl> */}
-
-                {/* Not removing date field as client might require this to be added */}
-
-                {/* <div className="inputFieldsForDateAndBtn">
-            <div
-              className=""
-              style={{ display: "flex", flexDirection: "column" }}
-            >
-              <TextField
-                name="date"
-                className="datePicker inputFields"
-                style={{ width: "40%", marginLeft: "2rem" }}
-                id="date"
-                label="Attempt Date"
-                type="date"
-                defaultValue="2017-05-24"
-                // className={`${classes.textField}`}
-                InputLabelProps={{
-                  shrink: true
-                }}
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                value={props.values.dateOfAttempt}
-              />
-              <small
-                style={{
-                  color: "red",
-                  marginLeft: "2rem"
-                }}
-              >
-                {props.errors.dateOfAttempt}
-              </small>
-            </div> */}
                 <Button
-                  // disabled={loading}
+                  disabled={loading}
                   type="submit"
-                  // onClick={onBtnClick}
+                  onClick={onBtnClick}
                   variant="contained"
                   text="Register"
                   style={{
                     width: "20%",
-                    // alignSelf: "left",
-                    // marginTop: "1rem",
                     marginLeft: "1.5rem"
                   }}
                 />
-                {/* <div className=""> */}
-                {/* <Button
-                  // onClick={onBtnClick}
-                  className="submit_btn"
-                  disabled={!(props.dirty && props.isValid)}
-                  type="submit"
-                  style={{
-                    backgroundColor: " #455ff0",
-                    width: "30%",
-                    alignSelf: "center",
-                    marginTop: "1.3rem",
-                    marginLeft: "2rem"
-                  }}
-                  variant="contained"
-                  color="primary"
-                >
-                  Register
-                </Button> */}
-                {/* </div> */}
-                {/* </div> */}
               </form>
               <small style={{ color: "red" }}>{alert}</small>
             </div>
