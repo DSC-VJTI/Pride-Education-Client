3e303d40b4bef8bffbbb5695bac6a9c3ea15ad83
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-04-04 16:54:51 +0530

    finish prod

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,7 @@
-export const BASE_URL = "https://pride-education.herokuapp.com/api";
+let URL = "http://localhost:3000/api";
+
+if (process.env.NODE_ENV === "production") {
+  URL = "https://pride-education.herokuapp.com/api";
+}
+
+export const BASE_URL = URL;
