223569ffe8fb6ff71c3f36ca0eaa8d159470177c
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-04-10 10:47:01 +0530

    change client to trigger workflow

--- client/src/components/ComingSoon.jsx
+++ client/src/components/ComingSoon.jsx
@@ -1,10 +1,11 @@
+import { Container } from "@material-ui/core";
 import React from "react";
 
 const ComingSoon = () => {
   return (
-    <>
+    <Container maxWidth="sm">
       <h1>Coming soon</h1>
-    </>
+    </Container>
   );
 };
 
