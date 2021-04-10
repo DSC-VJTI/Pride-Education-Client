832f02b09a6ea72cdc5859b04e760be3708b0c23
Tejas Jhamnani <tejasj2909@gmail.com>
2021-04-10 13:02:05 +0530

    Fixed Alignment of Cards

--- client/src/components/LandingPage/AniCard.js
+++ client/src/components/LandingPage/AniCard.js
@@ -5,7 +5,6 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import "./css/AniCard.css";
-import { Box } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
