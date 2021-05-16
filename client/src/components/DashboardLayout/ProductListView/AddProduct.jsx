a285bf3d2cfd8fb6b53b7c71e873ce22a22a8c57
pal-16 <50794716+pal-16@users.noreply.github.com>
2021-05-16 16:20:17 +0530

    integrating frontend and backend for image upload

--- client/src/components/DashboardLayout/ProductListView/AddProduct.jsx
+++ client/src/components/DashboardLayout/ProductListView/AddProduct.jsx
@@ -178,20 +178,11 @@ export default function AddProduct() {
           onSubmit={async (data, { setSubmitting, resetForm }) => {
             setSubmitting(true);
             // Preprocess Data
-            console.log("Data");
-            console.log(data);
             let formData = new FormData();
             let reqBody = { ...data };
-            console.log("Req Body");
-            console.log(reqBody["name"]);
-            console.log(reqBody["price"]);
-
             formData.append("name", reqBody["name"]);
             formData.append("price", reqBody["price"]);
             formData.append("discount", reqBody["discount"] || 0);
-            console.log("Form data");
-            console.log(formData.values);
-
             formData.append("imageUrl", file);
             switch (productType) {
               case "course":
