a49943fd1db5e9c6d3b88808955d0d93b0117dd8
BhavyaSheth22 <brsheth_b18@it.vjti.ac.in>
2021-05-23 12:26:12 +0530

    filter dropdown

--- client/src/components/Resource Page/ClassPane/ClassPane.js
+++ client/src/components/Resource Page/ClassPane/ClassPane.js
@@ -30,11 +30,25 @@ const ClassPaneStyles = makeStyles((theme) => ({
     },
     "& .rec.rec-arrow:hover": {
       background: "#f26522"
-    }
+    },
+    "& h2": {
+      fontSize: "2.8rem",
+      lineHeight: "3rem",
+      fontWeight: "600",
+      marginTop: "2rem",
+      color: "#333840",
+      letterSpacing: ".03rem",
+      marginBottom: ".5rem"
+    },
+    h6: {},
+    select: {}
+  },
+  adjustInputs: {
+    margin: "2rem 0"
   }
 }));
 
-const ClassPane = ({ Course, CoursesList }) => {
+const ClassPane = () => {
   const [products, setProducts] = useState({
     tests: [],
     books: [],
@@ -42,16 +56,18 @@ const ClassPane = ({ Course, CoursesList }) => {
   });
   const [subjects, setSubjects] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [subject, setSubject] = useState("CA");
 
   useEffect(() => {
     setIsLoading(true);
     getProducts();
   }, []);
+
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
-    { width: 600, itemsToShow: 2 },
-    { width: 900, itemsToShow: 3 },
-    { width: 1100, itemsToShow: 3 }
+    { width: 600, itemsToShow: 1 },
+    { width: 900, itemsToShow: 1 },
+    { width: 1100, itemsToShow: 2 }
   ];
 
   const classes = ClassPaneStyles();
@@ -67,7 +83,7 @@ const ClassPane = ({ Course, CoursesList }) => {
 
     allProducts.map((prod) => {
       if ("test" in prod) tests.push(prod);
-      else if ("course" in prod) {
+      else if ("course" in prod && prod.course.level.includes(subject)) {
         if (!uniqueSubjects.includes(prod.course.subject))
           uniqueSubjects.push(prod.course.subject);
         courses.push(prod);
@@ -84,10 +100,53 @@ const ClassPane = ({ Course, CoursesList }) => {
     setIsLoading(false);
   };
 
+  const onInputChange = (event) => {
+    setSubject(event.target.value);
+  };
+
   return isLoading ? (
     <Loading />
   ) : (
     <section>
+      <h6
+        style={{
+          fontSize: ".875rem",
+          letterSpacing: "1px",
+          color: "#333840",
+          fontWeight: "400",
+          lineHeight: "1.6"
+        }}
+      >
+        Course
+      </h6>
+      <div className="select">
+        <select
+          name="type"
+          id="type"
+          onChange={onInputChange}
+          style={{
+            padding: ".5rem .75rem",
+            lineHeight: "1.25",
+            background: "transparent",
+            border: "1px solid #979fa5",
+            height: "2.5rem",
+            paddingLeft: "1.2rem",
+            width: "50%",
+            maxWidth: "300px",
+            fontSize: "15px",
+            fontWeight: "300",
+            color: "#333840",
+            marginBottom: "0.5rem",
+            marginTop: "0.5rem",
+            "&:hover": {
+              border: "1px solid #000"
+            }
+          }}
+        >
+          <option value="CA">CA</option>
+          <option value="CA Final">CA Final</option>
+        </select>
+      </div>
       <ComboBox title="PEN DRIVE/GOOGLE DRIVE CLASSES" />
       <div>
         <ReactElasticCarousel
